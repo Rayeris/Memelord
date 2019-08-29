56bdfa5b038d4ada3bdaac4f47fbdc5e23280522
Rayeris <54676315+Rayeris@users.noreply.github.com>
2019-08-29 17:44:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function greet(name, lastName){
+    console.log('Labas, ' + name + ' ' + lastName);
+}
+greet('John', 'Smith');
+var username = prompt("What is your name?");
+let a = prompt("Kiek yra 14 + 6?");
